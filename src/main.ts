8370453700e457b25649487b4cd330ff1e6797a0
AntonioWingert <antoniobwingert@gmail.com>
2024-08-30 16:12:28 -0300

    fix: change starter port

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(bodyParser.json());
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 5000);
 }
 
 bootstrap();
